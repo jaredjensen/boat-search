94b8dd9aec395560af1646c27d0b4fd9e961e337
Jared Jensen <jared.jensen@forgerock.com>
2025-03-08 08:16:52 -0800

    Updates

--- boats.sh
+++ boats.sh
@@ -8,7 +8,7 @@ usage() {
 }
 
 outfile=boats.json
-distance=1500mi
+distance=5000mi
 length=38-50
 price=100000-400000
 year=1990-2015
@@ -29,11 +29,11 @@ done
 
 if [ $search -eq 1 ]; then
   echo "Refreshing search results..."
-  curl -s "https://www.yachtworld.com/yachtworld/search/boat?pageSize=200&page=1&facets=countrySubdivision,make,condition,makeModel,type,class,country,countryRegion,countryCity,fuelType,hullMaterial,hullShape,minYear,maxYear,minMaxPercentilPrices,enginesConfiguration,enginesDriveType,numberOfEngines&fields=id,make,model,year,featureType,specifications.dimensions.lengths.nominal,location.address,aliases,owner.logos,owner.name,owner.rootName,owner.location.address.city,owner.location.address.country,price.hidden,price.type.amount,portalLink,class,media,isOemModel,isCurrentModel,attributes,previousPrice,mediaCount,cpybLogo&useMultiFacetedFacets=true&enableSponsoredSearch=true&enablePremiumPlacement=false&enableOEM=true&locale=en-US&distance=$distance&radius=true&length=$length&uom=ft&sort=recommended&year=$year&fuelType=diesel&multiFacetedBoatTypeClass=\[%22power%22\]&price=$price&currency=USD&advantageSort=1&enableSponsoredSearchExactMatch=true&randomizedSponsoredBoatsSearch=true&randomSponsoredBoatsSize=0" -o $outfile
+  curl -s "https://www.yachtworld.com/yachtworld/search/boat?pageSize=400&page=1&facets=countrySubdivision,make,condition,makeModel,type,class,country,countryRegion,countryCity,fuelType,hullMaterial,hullShape,minYear,maxYear,minMaxPercentilPrices,enginesConfiguration,enginesDriveType,numberOfEngines&fields=id,make,model,year,featureType,specifications.dimensions.lengths.nominal,location.address,aliases,owner.logos,owner.name,owner.rootName,owner.location.address.city,owner.location.address.country,price.hidden,price.type.amount,portalLink,class,media,isOemModel,isCurrentModel,attributes,previousPrice,mediaCount,cpybLogo&useMultiFacetedFacets=true&enableSponsoredSearch=true&enablePremiumPlacement=false&enableOEM=true&locale=en-US&distance=$distance&radius=true&length=$length&uom=ft&sort=recommended&year=$year&fuelType=diesel&multiFacetedBoatTypeClass=\[%22power%22\]&price=$price&currency=USD&advantageSort=1&enableSponsoredSearchExactMatch=true&randomizedSponsoredBoatsSearch=true&randomSponsoredBoatsSize=0" -o $outfile
 fi
 
 # Create boats.js
-ignore='"Bayliner","Carver","North Pacific","Sea Ray","Mainship","Meridian","Navigator","Formula","Fountain","DeFever","Silverton"'
+ignore='"Bayliner","Carver","North Pacific","Sea Ray","Mainship","Meridian","Navigator","Formula","Fountain","DeFever","Silverton","Bertram","Jersey","Riviera","Tiara","Tiara Yachts","Beneteau","Post","Viking","Pursuit","Regal","Azimut","Maxum"'
 query="[[.search.records[] | select(.make | IN($ignore) | not)][] | { id: .id, type: ((.year|tostring) + \" \" + .make + \" \" + .model), price: .price.type.amount.USD, length: .specifications.dimensions.lengths.nominal.ft, location: (.location.address.city + \" \" + .location.address.subdivision), url: .portalLink, image: .media[0].url }]"
 echo "const boats = $(jq -r "$query" $outfile)" > boats.js
 
