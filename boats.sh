c3d21b613e99b810f651517e2b5c7ec91ef4bc47
Jared Jensen <jared.jensen@forgerock.com>
2025-03-08 09:17:41 -0800

    Updates

--- boats.sh
+++ boats.sh
@@ -7,11 +7,13 @@ usage() {
     exit 1
 }
 
+jsfile=boats.js
 outfile=boats.json
-distance=5000mi
+distance=1500mi
 length=38-50
 price=100000-400000
 year=1990-2015
+pagesize=400
 
 # Flags
 open=0
@@ -27,15 +29,34 @@ do
     shift
 done
 
+echo "const boats = [" > $jsfile
+
 if [ $search -eq 1 ]; then
   echo "Refreshing search results..."
-  curl -s "https://www.yachtworld.com/yachtworld/search/boat?pageSize=400&page=1&facets=countrySubdivision,make,condition,makeModel,type,class,country,countryRegion,countryCity,fuelType,hullMaterial,hullShape,minYear,maxYear,minMaxPercentilPrices,enginesConfiguration,enginesDriveType,numberOfEngines&fields=id,make,model,year,featureType,specifications.dimensions.lengths.nominal,location.address,aliases,owner.logos,owner.name,owner.rootName,owner.location.address.city,owner.location.address.country,price.hidden,price.type.amount,portalLink,class,media,isOemModel,isCurrentModel,attributes,previousPrice,mediaCount,cpybLogo&useMultiFacetedFacets=true&enableSponsoredSearch=true&enablePremiumPlacement=false&enableOEM=true&locale=en-US&distance=$distance&radius=true&length=$length&uom=ft&sort=recommended&year=$year&fuelType=diesel&multiFacetedBoatTypeClass=\[%22power%22\]&price=$price&currency=USD&advantageSort=1&enableSponsoredSearchExactMatch=true&randomizedSponsoredBoatsSearch=true&randomSponsoredBoatsSize=0" -o $outfile
+  page=1
+  while true; do
+    printf "$page"
+    curl -s "https://www.yachtworld.com/yachtworld/search/boat?pageSize=$pagesize&page=$page&facets=countrySubdivision,make,condition,makeModel,type,class,country,countryRegion,countryCity,fuelType,hullMaterial,hullShape,minYear,maxYear,minMaxPercentilPrices,enginesConfiguration,enginesDriveType,numberOfEngines&fields=id,make,model,year,featureType,specifications.dimensions.lengths.nominal,location.address,aliases,owner.logos,owner.name,owner.rootName,owner.location.address.city,owner.location.address.country,price.hidden,price.type.amount,portalLink,class,media,isOemModel,isCurrentModel,attributes,previousPrice,mediaCount,cpybLogo&useMultiFacetedFacets=true&enableSponsoredSearch=false&enablePremiumPlacement=false&enableOEM=true&locale=en-US&distance=$distance&radius=true&length=$length&uom=ft&sort=recommended&year=$year&fuelType=diesel&multiFacetedBoatTypeClass=\[%22power%22\]&price=$price&currency=USD&advantageSort=1&enableSponsoredSearchExactMatch=true&randomizedSponsoredBoatsSearch=false&randomSponsoredBoatsSize=0" -o $outfile
+
+    records=$(jq -r '.search.records | length' $outfile)
+    if [ $? -ne 0 ]; then
+      echo "Failed to parse JSON"
+      break
+    elif [ "$records" -eq "0" ]; then
+      break
+    fi
+
+    ignore='"Azimut","Bayliner","Beneteau","Bertram","Carver","Cruisers Yachts","DeFever","Formula","Fountain","Jersey","Mainship","Maxum","Meridian","Navigator","North Pacific","Post","Pursuit","Regal","Riviera","Sea Ray","Silverton","Tiara Yachts","Tiara","Viking"'
+    query="[.search.records[] | select(.make | IN($ignore) | not)][] | { id: .id, type: ((.year|tostring) + \" \" + .make + \" \" + .model), price: .price.type.amount.USD, length: .specifications.dimensions.lengths.nominal.ft, location: (.location.address.city + \" \" + .location.address.subdivision), url: .portalLink, image: .media[0].url }"
+    jq -r "$query" $outfile >> $jsfile
+
+    ((page++))
+    printf "."
+  done
 fi
 
-# Create boats.js
-ignore='"Bayliner","Carver","North Pacific","Sea Ray","Mainship","Meridian","Navigator","Formula","Fountain","DeFever","Silverton","Bertram","Jersey","Riviera","Tiara","Tiara Yachts","Beneteau","Post","Viking","Pursuit","Regal","Azimut","Maxum"'
-query="[[.search.records[] | select(.make | IN($ignore) | not)][] | { id: .id, type: ((.year|tostring) + \" \" + .make + \" \" + .model), price: .price.type.amount.USD, length: .specifications.dimensions.lengths.nominal.ft, location: (.location.address.city + \" \" + .location.address.subdivision), url: .portalLink, image: .media[0].url }]"
-echo "const boats = $(jq -r "$query" $outfile)" > boats.js
+echo "];" >> $jsfile
+sed -i '' -e 's/}/},/g' $jsfile
 
 if [ $open -eq 1 ]; then
   open boats.html
